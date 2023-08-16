9a621e0d4ba2bc0f31115050ec5e90ab24b954cd
Alyx <figments@outlook.com>
2023-08-15 18:21:05 -0700

    Initial Commit

---
+++ Darterfish/DarterfishApp.swift
@@ -0,0 +1,17 @@
+//
+//  DarterfishApp.swift
+//  Darterfish
+//
+//  Created by Alyx Mote on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct DarterfishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
